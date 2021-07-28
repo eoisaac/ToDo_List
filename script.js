c5cf0fe43b81a59c04edee376e08d6aaa4f0133a
isaac <isaacsgpz@gmail.com>
2021-07-28 09:46:40 -0300

    Add marcar and apagar functions

--- script.js
+++ script.js
@@ -2,22 +2,33 @@ const inptTarefa = document.querySelector('#inptTarefa');
 const botaoAdd = document.querySelector('#botaoAdd');
 const listaTarefas = document.querySelector('#listaTarefas');
 
+//Cria botão para marcar a <li>
+function addBotaoMarcar(li){
+	const btnMarcar = document.createElement('button');
+	btnMarcar.innerHTML = '✔';
+	btnMarcar.setAttribute('id', 'idMarcar');
+	btnMarcar.setAttribute('class', 'classMarcar');
+	btnMarcar.setAttribute('title', 'Concluir tarefa');
+	li.appendChild(btnMarcar);
+}
+
+//Cria botão para apagar a <li>
+function addBotaoApagar(li){
+	const btnApagar = document.createElement('button');
+	btnApagar.innerHTML = 'X';
+	btnApagar.setAttribute('id', 'idApagar');
+	btnApagar.setAttribute('class', 'classApagar');
+	btnApagar.setAttribute('title', 'Apagar tarefa');
+	li.appendChild(btnApagar);
+}
 
 function criaTarefa(tarefaValue){
 	//Cria <li> com a tarefa;
 	const liElement = document.createElement('li');
 	liElement.innerText = tarefaValue;
 	listaTarefas.appendChild(liElement);
-
-	//Cria botão para marcar a <li>
-	const btnMarcar = document.createElement('button');
-	btnMarcar.innerHTML = '✔'
-	liElement.appendChild(btnMarcar);
-
-	//Cria botão para apagar a <li>
-	const btnApagar = document.createElement('button');
-	btnApagar.innerHTML = 'X'
-	liElement.appendChild(btnApagar);
+	addBotaoMarcar(liElement);
+	addBotaoApagar(liElement);
 }
 
 
